b97d24087ab96802a8faa7d543267ff6dce07c52
lemonstener <deyanvasilev7@gmail.com>
2022-01-30 14:39:39 -0600

    sorts

---
+++ practice/sorting-algorithms/app.js
@@ -0,0 +1,84 @@
+const bubbleSort = (arr) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      count++;
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  console.log(`COUNT: ${count}`);
+  return arr;
+};
+
+const bubbleSort2 = (arr) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      count++;
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  console.log(`COUNT: ${count}`);
+  return arr;
+};
+
+const bubbleSort3 = (arr) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let swapped = false;
+    for (let j = 0; j < arr.length; j++) {
+      count++;
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
+      if (!swapped) break;
+    }
+  }
+  console.log(`COUNT: ${count}`);
+  return arr;
+};
+
+const merge = (arr1, arr2) => {
+  const result = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result;
+};
+
+[3, 2, 7, 1];
+
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+};
